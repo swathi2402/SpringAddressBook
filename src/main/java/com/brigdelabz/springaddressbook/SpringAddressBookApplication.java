db8eaa94b47325308ad2ab7f34a20a8e6adf9207
Swathi <swathi.h@perfios.com>
2021-11-05 14:30:47 +0530

    ADDED: Lombok Library used for logging

--- src/main/java/com/brigdelabz/springaddressbook/SpringAddressBookApplication.java
+++ src/main/java/com/brigdelabz/springaddressbook/SpringAddressBookApplication.java
@@ -3,11 +3,15 @@ package com.brigdelabz.springaddressbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class SpringAddressBookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAddressBookApplication.class, args);
+		log.info("Address Book App Started");
 	}
 
 }
