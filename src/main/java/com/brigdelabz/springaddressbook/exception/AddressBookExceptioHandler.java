4c45925f06d93bcbe3e9c22bc6cea442db89bdaf
Swathi <swathi.h@perfios.com>
2021-11-05 16:11:02 +0530

    ADDED: Class to give user friendly error when validation fails

---
+++ src/main/java/com/brigdelabz/springaddressbook/exception/AddressBookExceptioHandler.java
@@ -0,0 +1,27 @@
+package com.brigdelabz.springaddressbook.exception;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.brigdelabz.springaddressbook.dto.ResponseDTO;
+
+@ControllerAdvice
+public class AddressBookExceptioHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errorMessage = errorList.stream()
+									.map(objErr -> objErr.getDefaultMessage())
+									.collect(Collectors.toList());
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request", errorMessage);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+}
