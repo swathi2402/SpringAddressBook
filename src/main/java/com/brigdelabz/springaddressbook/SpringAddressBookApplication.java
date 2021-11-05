d6657e88863f5e5944c342afcceef7a0b6562eec
Swathi <swathi.h@perfios.com>
2021-11-05 14:49:20 +0530

    ADDED: Database settings as environmental variable

--- src/main/java/com/brigdelabz/springaddressbook/SpringAddressBookApplication.java
+++ src/main/java/com/brigdelabz/springaddressbook/SpringAddressBookApplication.java
@@ -13,6 +13,6 @@ public class SpringAddressBookApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringAddressBookApplication.class, args);
 		log.info("Address Book App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+		log.info("Address Book DB User is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
-
 }
