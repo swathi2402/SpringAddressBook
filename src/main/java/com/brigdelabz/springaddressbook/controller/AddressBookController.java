6226d015bc2e9817a4d5b25f2d95839f1149120e
Swathi <swathi.h@perfios.com>
2021-11-05 16:04:26 +0530

    ADDED: Validations to fields to validate REST calls

--- src/main/java/com/brigdelabz/springaddressbook/controller/AddressBookController.java
+++ src/main/java/com/brigdelabz/springaddressbook/controller/AddressBookController.java
@@ -2,6 +2,8 @@ package com.brigdelabz.springaddressbook.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -48,7 +50,7 @@ public class AddressBookController {
 	}
 
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDTO> addAddressBookData(@RequestBody ContactDTO contactDTO) {
+	public ResponseEntity<ResponseDTO> addAddressBookData(@Valid @RequestBody ContactDTO contactDTO) {
 		AddressBookData addressBookData = null;
 		addressBookData = addressBookService.createAddressBookData(contactDTO);
 		ResponseDTO responseDTO = new ResponseDTO("Created Contact Data for: ", addressBookData);
@@ -57,7 +59,7 @@ public class AddressBookController {
 
 	@PutMapping("/update/{contactId}")
 	public ResponseEntity<ResponseDTO> updateAddressBookData(@PathVariable("contactId") int contactId,
-			@RequestBody ContactDTO contactDTO) {
+			@Valid @RequestBody ContactDTO contactDTO) {
 		AddressBookData addressBookData = null;
 		addressBookData = addressBookService.updateAddressBookData(contactId, contactDTO);
 		ResponseDTO responseDTO = new ResponseDTO("Updated Contact Data for: ", addressBookData);
--- src/main/java/com/brigdelabz/springaddressbook/dto/ContactDTO.java
+++ src/main/java/com/brigdelabz/springaddressbook/dto/ContactDTO.java
@@ -1,8 +1,12 @@
 package com.brigdelabz.springaddressbook.dto;
 
+import javax.validation.constraints.Pattern;
+
 public class ContactDTO {
 
+	@Pattern(regexp = "^[A-Z]{1}[a-zA_Z\\s]{2,}$", message = "Contact name Invalid")
 	public String name;
+	@Pattern(regexp = "'^([+]?[1-9][0-9])?[0-9]{10}$'", message = "Ph Invalid")
 	public String phoneNumber;
 
 	public ContactDTO(String name, String phoneNumber) {
